16eff8b8b866b66882ac73ff3d717149bec06666
Arahk Yambupah <arahk@outlook.com>
2022-09-19 13:28:36 +0700

    Add Validations

--- BBL.ModernApp.AuditAggregate.Contracts/PayloadMessage.cs
+++ BBL.ModernApp.AuditAggregate.Contracts/PayloadMessage.cs
@@ -1,36 +1,95 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using System.Text.Json.Serialization;
 
 namespace BBL.ModernApp.AuditAggregate.Contracts
 {
     public sealed class PayloadMessage
-    {
+    {        
+        [MaxLength(50)]
         public string? SessionID { get; internal set; }
-        public DateTime OperationDateTime { get; internal set; }
+
+        public DateTime OperationDateTime { get; internal set; } = DateTime.MinValue;
+
+        [MaxLength(32)]
         public string OperationName { get; internal set; } = null!;
+
+        [MaxLength(32)]
         public string CustomerID { get; internal set; } = null!;
+
+        [MaxLength(50)]
         public string Channel { get; internal set; } = null!;
+
+        [MaxLength(50)]
         public string? SubChannel { get; internal set; }
-        public int? DeviceID { get; internal set; }
+                
+        public Int32? DeviceID { get; internal set; }
+
+        [MaxLength(32)]
         public string? IPAddress { get; internal set; }
+
         public bool Succeeded { get; internal set; }
+
+        [MaxLength(255)]
         public string? ErrorCode { get; internal set; }
+
+        [MaxLength(100)]
         public string? Info1 { get; internal set; }
+
+        [MaxLength(100)]
         public string? Info2 { get; internal set; }
+
+        [MaxLength(100)]
         public string? Info3 { get; internal set; }
+
+        [MaxLength(100)]
         public string? Info4 { get; internal set; }
+
+        [MaxLength(100)]
         public string? Info5 { get; internal set; }
+
+        [MaxLength(100)]
         public string? Info6 { get; internal set; }
+
+        [MaxLength(255)]
         public string? Info7 { get; internal set; }
+
+        [MaxLength(255)]
         public string? Info8 { get; internal set; }
+
+        [MaxLength(255)]
         public string? Info9 { get; internal set; }
+
+        [MaxLength(255)]
         public string? Info10 { get; internal set; }
+
+        [MaxLength(255)]
         public string? Keyword { get; internal set; }
+
+        [MaxLength(1000)]
         public string? DisplayMessage { get; internal set; } = null!;
 
         [JsonConstructor]
         internal PayloadMessage() { }
 
+        /// <summary>
+        /// This is only way to create Payload Message
+        /// </summary>
+        /// <param name="operationName"></param>
+        /// <param name="operationDateTime"></param>
+        /// <param name="channel"></param>
+        /// <param name="customerId"></param>
+        /// <param name="isSuccess"></param>
+        /// <param name="sessionId"></param>
+        /// <param name="subChannel"></param>
+        /// <param name="deviceId"></param>
+        /// <param name="ipAddress"></param>
+        /// <param name="errorCode"></param>
+        /// <param name="keyword"></param>
+        /// <param name="displayMessage"></param>
+        /// <param name="infos"></param>
+        /// <returns></returns>
+        /// <exception cref="PayloadMessageInvalidException"></exception>
         public static PayloadMessage New(
             string operationName
             , DateTime operationDateTime
@@ -88,7 +147,41 @@ namespace BBL.ModernApp.AuditAggregate.Contracts
                 }
             }
 
+            List<ValidationResult> validationResults = payloadMessage.Validate(out bool isValid);
+
+            if (!isValid)
+            {
+                throw new PayloadMessageInvalidException(validationResults);
+            }
+
             return payloadMessage;
+        }
+
+        public List<ValidationResult> Validate(out bool isValid)
+        {
+            ValidationContext validationContext = new(this);
+            List<ValidationResult> validationResult = new();
+
+            if(OperationDateTime == DateTime.MinValue)
+            {
+                validationResult.Add(new ValidationResult("Cannot equal to DateTime.MinValue", new[] { nameof(OperationDateTime) }));
+            }
+
+            if(IPAddress != null)
+            {
+                try
+                {
+                    Uri _ = new (IPAddress);
+                }
+                catch
+                {
+                    validationResult.Add(new ValidationResult("IP Address not valid URI format.", new[] { nameof(IPAddress) }));
+                }
+            }
+
+            isValid = Validator.TryValidateObject(this, validationContext, validationResult);
+
+            return validationResult;
         }
     }
 }
---
+++ BBL.ModernApp.AuditAggregate.Contracts/PayloadMessageInvalidException.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BBL.ModernApp.AuditAggregate.Contracts
+{
+    [Serializable]
+    public sealed class PayloadMessageInvalidException : Exception
+    {
+        private List<ValidationResult> validationResults;
+        public List<ValidationResult> ValidationResults { get => validationResults; }
+
+        public PayloadMessageInvalidException(List<ValidationResult> validationResults)
+             : base("Payload Creation has Invalid Properties Value.")
+        {
+            this.validationResults = validationResults;
+        }
+
+    }
+}
