d8230e10aaad37b1ec00971143f09369843ac1f1
Arahk Yambupah <arahk@outlook.com>
2022-10-17 12:07:31 +0700

    Test Multi Thread Producer

--- ConsoleAppProduce/Program.cs
+++ ConsoleAppProduce/Program.cs
@@ -3,6 +3,7 @@ using BBL.ModernApp.AuditAggregate.Contracts;
 using BBL.ModernApp.AuditAggregate.Contracts.Producer;
 
 Console.WriteLine("Begin Produce.");
+MessageProducer producer = new("kafka01:9092", "MFHost");
 
 while (true)
 {
@@ -10,22 +11,24 @@ while (true)
 
     string? userEntry = Console.ReadLine();
 
-    if (string.IsNullOrEmpty(userEntry)) continue;    
+    if (string.IsNullOrEmpty(userEntry)) continue;
 
-    var payloadMessage = PayloadMessage.New("TestAuditLog", 
-        DateTime.Now, 
-        "ConsoleAppProduce", 
-        "ConsoleAppProduceClient", 
+    for (int i = 0; i < 100; i++)
+    {
+        var payloadMessage = PayloadMessage.New("TestAuditLog",
+        DateTime.Now,
+        "ConsoleAppProduce",
+        "ConsoleAppProduceClient",
         true,
         errorCode: "N/A",
         deviceId: 123456,
         ipAddress: "127.0.0.1",
         subChannel: "Console Application",
         sessionId: Guid.NewGuid().ToString(),
-        displayMessage: userEntry,
+        displayMessage: userEntry + "_" + (i + 1).ToString(),
         keyword: "MFHost",
         infos: new[] { "info01", "info02", "info03", "info04", "info05", "info06", "info07", "info08", "info09", "info10" });
 
-    MessageProducer producer = new("kafka01:9092", "MFHost");
-    producer.Send(payloadMessage);
+        producer.Send(payloadMessage);
+    }
 }
